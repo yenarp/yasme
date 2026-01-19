3c8df6c7205abb27c6694e6bb6c788f4a877eb2a
mdenjoyer <mdenjoyer@proton.me>
2026-01-19 17:37:04 +0100

    feat: EM64T mov reg, imm

--- stl/arch/EM64T/emit.asm
+++ stl/arch/EM64T/emit.asm
@@ -1,3 +1,5 @@
+; include 'util.asm'
+
 macro emit_operand_size_override
 	db 0x66
 end macro
@@ -37,5 +39,45 @@ macro emit_mov_rm size
 	if size == 8
 		emit_rex_w
 		db 0x8B
+	end if
+end macro
+
+macro emit_mov_imm dstnum, size
+	if size == 1
+		evalwrap op, 0xB0+dstnum
+		db op
+	end if
+
+	if size == 2
+		emit_operand_size_override
+		evalwrap op, 0xB8+dstnum
+		db op
+	end if
+
+	if size == 4
+		evalwrap op, 0xB8+dstnum
+		db op
+	end if
+
+	if size == 8
+		emit_rex_w
+		evalwrap op, 0xB8+dstnum
+		db op
+	end if
+end macro
+
+; TODO maybe refactor to somewhere common in the STL
+macro emit_imm imm, size
+	if size == 1
+		db imm
+	end if
+	if size == 2
+		dw imm
+	end if
+	if size == 4
+		dd imm
+	end if
+	if size == 8
+		dq imm
 	end if
 end macro
--- stl/arch/EM64T/mov.asm
+++ stl/arch/EM64T/mov.asm
@@ -2,9 +2,19 @@ include 'util.asm'
 include 'reg.asm'
 include 'emit.asm'
 
+macro mov_reg_imm tokens t
+	match t, {dst}, {imm}
+		local dstnum
+		local size
+		get_reg_num dstnum, size, dst
+
+		emit_mov_imm dstnum, size
+		emit_imm imm, size
+	end match
+end macro
+
 macro mov_reg_reg tokens t
 	match t, {dst}, {src}
-
 		local dstnum
 		local dstsize
 		get_reg_num dstnum, dstsize, dst
@@ -15,8 +25,8 @@ macro mov_reg_reg tokens t
 		evalwrap modrm, 0b11<<6|(dstnum)|(srcnum<<3)
 
 		if dstsize - srcsize
-			error "Invalid operand sizes", t
-			end error
+			; error "Invalid operand sizes", t
+			; end error
 		end if
 
 		emit_mov_mr srcsize
@@ -39,8 +49,8 @@ macro mov_deref_reg_reg tokens t
 		evalwrap modrm, 0b00<<6|(dstnum)|(srcnum<<3)
 
 		if dstsize - srcsize
-			error "Invalid operand sizes", t
-			end error
+			; error "Invalid operand sizes", t
+			; end error
 		end if
 
 		emit_mov_mr srcsize
@@ -63,8 +73,8 @@ macro mov_reg_deref_reg tokens t
 		evalwrap modrm, 0b00<<6|(dstnum<<3)|(srcnum)
 
 		if dstsize - srcsize
-			error "Invalid operand sizes", t
-			end error
+			; error "Invalid operand sizes", t
+			; end error
 		end if
 
 		emit_mov_rm srcsize
@@ -86,7 +96,16 @@ macro mov tokens t
 				mov_deref_reg_reg t
 			else
 				match t, {dst}, {src}
-					mov_reg_reg t
+					local srcnum
+					local srcsize
+					get_reg_num srcnum, srcsize, src
+
+					if srcnum == 0xFF ; immediate
+						mov_reg_imm t
+					else
+						mov_reg_reg t
+					end if
+
 				end match
 			end match
 		end match
